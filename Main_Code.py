f4f3906163acf771f96641f39b55c04a1c7e965f
Jin-Moa <91327226+Jin-mo@users.noreply.github.com>
2023-07-01 20:40:12 +0300

    Update Main_Code.py

--- Main_Code.py
+++ Main_Code.py
@@ -6,19 +6,19 @@ cwd = os.getcwd()
 if platform.system() == "Windows":
     
     print("We're in Windows")
-    os.chdir("Change to the directory/location where the file/folder is")
+    os.chdir("Change to the directory/location where the file/folder is") # Change to the directory/location where the file/folder is
     cwd = "Location of specified Directory"
 
 elif platform.system() == "Linux":
     
     print("We're in Linux")
-    os.chdir("Change to the directory/location where the file/folder is")
+    os.chdir("Change to the directory/location where the file/folder is") # Change to the directory/location where the file/folder is
     cwd = "Location of specified Directory" 
 
 if cwd == "The directory/location from where the python script is being executed": #This is to make sure the script doesn't auto delete itself and any other files
     exit()
 
-elif cwd == "Location of specified Directory" or cwd == "Location of specified Directory":
+elif cwd == "Location of specified Directory" or cwd == "Location of specified Directory": # Location of specified Directory
     lst = []
     for file in os.listdir():
         lst.append(file)
